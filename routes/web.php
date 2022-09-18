5403df1f0dd2e5f32e58824a48eb6832a0aff524
Tengku Surya <82017653+suryun-ni@users.noreply.github.com>
2022-09-18 10:03:40 +0700

    pemesanan controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\pemesanan_controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/pemesanan/list',[pemesanan_controller::class,'index']);
